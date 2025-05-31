d91c5dd5f61f8605b1f06fe6a2be3d856015595b
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-06-01 00:48:25 +0600

    Learn: hanler and route system

--- Express101/index.js
+++ Express101/index.js
@@ -1,15 +1,47 @@
-
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.send("Hello world")
-})
+app.get("/", (req, res) => {
+  res.send(
+    `
+        <!DOCTYPE html>
+            <html lang="en">
+            <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Nodejs you are Awsome!!</title>
+            <style>
+            .container{
+                background-color: #dddeee;
+                padding: 20px;
+                border-radius: 20px;
+            }
+            </style>
+            </head>
+            <body>
+
+            <div class="container">
+            <h2>Hello! Nodejs you are Awsome!!</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatem optio id! Laudantium officia tempore ab hic suscipit, tenetur nam quas nemo, ullam alias ipsam ad perspiciatis, accusamus nobis ea!</p>
+            </div>
+
+            </body>
+            </html>
+                    
+        `
+  ); 
+});
+
+app.listen(4000, (req, res) => {
+  console.log("Server listing  On PORT 4000");
+});
 
 
-app.listen(5000,(req,res) =>{
-    console.log('Server listing on 5000');
-})
+function handler(req, res, next){
+    // read request object
+    // process request
+    // response back the result
+}
