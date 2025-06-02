49fd75fafb38f608b696c668d843b9d0a425db28
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-06-02 18:46:40 +0600

    Learn : Route

--- Express101/index.js
+++ Express101/index.js
@@ -4,36 +4,27 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send(
-    `
-        <!DOCTYPE html>
-            <html lang="en">
-            <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Nodejs you are Awsome!!</title>
-            <style>
-            .container{
-                background-color: #dddeee;
-                padding: 20px;
-                border-radius: 20px;
-            }
-            </style>
-            </head>
-            <body>
-
-            <div class="container">
-            <h2>Hello! Nodejs you are Awsome!!</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatem optio id! Laudantium officia tempore ab hic suscipit, tenetur nam quas nemo, ullam alias ipsam ad perspiciatis, accusamus nobis ea!</p>
-            </div>
-
-            </body>
-            </html>
-                    
-        `
-  ); 
+// Your existing routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the homepage!');
 });
+
+app.get('/about', (req, res) => {
+  res.send('This is the about page.');
+});
+
+
+app.get('/help', (req, res) => {
+  res.send('This is the help page.');
+});
+
+
+
+// 404 Handler (Always keep at the end)
+app.use((req, res, next) => {
+  res.status(404).send("Hello ami error aisa porse!")
+})
+
 
 app.listen(4000, (req, res) => {
   console.log("Server listing  On PORT 4000");
