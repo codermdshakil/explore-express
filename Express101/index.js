dd7ec235f41f60bf77cd0c5888afe8a1fa6a2218
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-06-02 22:08:46 +0600

    Learn : Response objects properties and methods

--- Express101/index.js
+++ Express101/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fs = require('fs');
+const fs = require("fs");
 const path = require("path");
 
 const app = express();
@@ -8,40 +8,59 @@ app.use(express.json());
 
 // Your existing routes
 
-
 // res.status
 // res.send
 
-app.get('/', (req,res) => {
-  res.status(200).send("Hello world")
-})
+app.get("/", (req, res) => {
+  res.status(200).send("Hello world");
+});
 
 // res.sendFile using these we can sent a file from server to client
-app.get('/sendfile', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'index.html'))
-})
+app.get("/sendfile", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "index.html"));
+});
 
+// res.append this value shown in header section
+app.get("/explore", (req, res) => {
+  res.append("Link", ["<http://localhost/>", "<http://localhost:3000/>"]);
+  res.append("Set-Cookie", "foo=bar; Path=/; HttpOnly");
+  res.append("Warning", "199 Miscellaneous warning");
 
+  res.format({
+    text() {
+      res.send("hey");
+    },
 
+    html() {
+      res.send("<p>hey</p>");
+    },
 
-// app.get('/', (req, res) => {
+    json() {
+      res.send({ message: "hey" });
+    }
 
-//   fs.readFile('./pages/index.html', (err, data) =>{
+  });
 
-//     if(err){
-//       console.log('Error');
-//       res.send('<h1>Something went wrong</h1>')
-//     }
-//     else{
-//       res.write(data)
-//       res.end()
-//     }
-//   })
-//   // res.send('Welcome to the homepage!');
-// });
+  console.log(res.get('Content-Type'));
 
+});
 
-// res.download example 
+// res.cookie set in cookie section
+app.get("/cookie", (req, res) => {
+  res.cookie("name", "tobi", {
+    domain: ".example.com",
+    path: "/admin",
+    secure: true,
+  });
+  res.cookie(
+    "ShakilAhmed",
+    { name: "Shakil Ahmed", age: 21, profession: "Software Developer" },
+    { expires: new Date(Date.now() + 900000), httpOnly: true }
+  );
+  res.send("Cookies");
+});
+
+// res.download example
 // app.get('/about', (req, res) => {
 //   res.download('./img/mql5.png', (err) => {
 //     if (err) {
@@ -50,35 +69,43 @@ app.get('/sendfile', (req, res) => {
 //   });
 // });
 
-
 // res.redirect example -
 // app.get('/about', (req, res) => {
 //   res.redirect('/help')
 // });
 
-
-
-
-
-app.get('/help', (req, res) => {
-  res.send('This is the help page.');
+app.get("/help", (req, res) => {
+  res.send("This is the help page.");
 });
-
-
 
 // 404 Handler (Always keep at the end)
 app.use((req, res, next) => {
-  res.status(404).send("Hello ami error aisa porse!")
-})
-
+  res.status(404).send("Hello ami error aisa porse!");
+});
 
 app.listen(4000, (req, res) => {
   console.log("Server listing  On PORT 4000");
 });
 
-
-function handler(req, res, next){
-    // read request object
-    // process request
-    // response back the result
+function handler(req, res, next) {
+  // read request object
+  // process request
+  // response back the result
 }
+
+
+// summary 
+// Method	Description & Example
+// res.send() -> 	Sends a plain text or HTML response.  📝 res.send('Hello World')
+// res.json()	-> Sends a JSON response.  📝 res.json({ name: 'John' })
+// res.status() -> 	Sets HTTP status code.  📝 res.status(404).send('Not found')
+// res.sendStatus() ->	Sets status code and sends its string.  📝 res.sendStatus(200) → OK
+// res.set() ->	Sets a response header.  📝 res.set('Content-Type', 'text/plain')
+// res.get() ->	Gets the value of a response header.
+// res.cookie() -> 	Sets a cookie.  📝 res.cookie('token', 'abc123')
+// res.clearCookie() -> 	Clears a cookie.
+// res.redirect() ->	Redirects to another URL.  📝 res.redirect('/login')
+// res.render()	 -> Renders a view template (e.g., EJS, Pug).  📝 res.render('home', { title: 'Home' })
+// res.download() -> 	Sends a file for download.
+// res.sendFile() -> 	Sends a file as response.  📝 res.sendFile(__dirname + '/file.pdf')
+// res.end() ->	Ends the response process.
