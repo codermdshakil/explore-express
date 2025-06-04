2b7c93c87ba3a604718e72c8f8ce490713c9c0cc
Md. Shakil Ahmed <86945977+codermdshakil@users.noreply.github.com>
2025-06-04 17:30:23 +0600

    Handle: Not found page error and Global error

--- raffle-draw-project/src/server.js
+++ raffle-draw-project/src/server.js
@@ -1,20 +1,37 @@
-
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 
+// handle middlewares
+app.use([morgan("dev"), cors(), express.json()]);
 
-app.get('/', (req, res, next) => {
-    res.send('Hello world')
-})
+app.get("/", (req, res, next) => {
+  res.send("Hello world");
+});
 
-app.get('/health', (req, res) => {
-    res.send("OK")
-})
+app.get("/health", (req, res) => {
+  res.status(200).send({ message: "Ok" });
+});
 
+// Not found 404 error handle
+app.use((req, res, next) => {
+  const error = new Error("Resources not found");
+  error.status = 404;
+  next(error);
+});
+
+// hanld global error
+app.use((err, req, res, next) => {
+  if (err.status) {
+    return res.status(err.status).json({ message: err.message });
+  }
+
+  res.status(500).json({message:"Something want wrong!"})
+});
 
 
 app.listen(5000, (req, res) => {
-    console.log('Server is runing on port 5000');
-})
-
+  console.log("Server is runing on port 5000");
+});
